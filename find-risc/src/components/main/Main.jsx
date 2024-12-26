a7da04a852587e55cab0b3f372c33e3096788ac8
João Miguel Santos <miguel.santos@dcx.ufpb.br>
2024-12-26 16:35:54 -0300

    Adcionando o modal no formulário
    O botão de calcular o imc foi adcionando na questão do formulário correspondente, e troquei também o rodapé para o  lugar correto

--- find-risc/src/components/main/Main.jsx
+++ find-risc/src/components/main/Main.jsx
@@ -1,21 +1,13 @@
-import { useState } from "react";
+
 import "./Main.css";
-import Modal from "../modal/Modal";
 import Form from "../form/Form";
 
 const Main = () => {
-  const [open, setOpen] = useState(false);
+ 
 
   return (
     <>
       <h1>Conteudo Principal</h1>
-      <br />
-      <br />
-
-      <h3>Imc</h3>
-      <button onClick={() => setOpen(!open)}>Calcule imc</button>
-      <Modal isOpen={open} set={setOpen} />
-
       <Form />
     </>
   );
